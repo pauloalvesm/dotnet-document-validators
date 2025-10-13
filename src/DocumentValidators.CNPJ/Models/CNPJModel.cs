693bb19a5743429d61519ec9f520799b49986fc3
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-13 20:57:09 -0300

    feat: initial version to validate CNPJ

---
+++ src/DocumentValidators.CNPJ/Models/CNPJModel.cs
@@ -0,0 +1,53 @@
+﻿namespace DocumentValidators.CNPJ.Models;
+
+public class CNPJModel
+{
+    public static bool ValidateCnpj(string cnpj)
+    {
+        if (string.IsNullOrEmpty(cnpj)) 
+        {
+            return false;
+        }
+            
+
+        cnpj = new string(cnpj.Where(char.IsDigit).ToArray());
+
+        if (cnpj.Length != 14) 
+        {
+            return false;
+        }
+
+        if (cnpj.Distinct().Count() == 1) 
+        {
+            return false;
+        }
+
+        int[] multiplier1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] multiplier2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        string tempCnpj = cnpj.Substring(0, 12);
+        int sum = 0;
+
+        for (int i = 0; i < 12; i++) 
+        {
+            sum += (tempCnpj[i] - '0') * multiplier1[i];
+        }
+            
+        int remainder = sum % 11;
+        int digit1 = (remainder < 2) ? 0 : 11 - remainder;
+
+        tempCnpj += digit1;
+        sum = 0;
+
+        for (int i = 0; i < 13; i++) 
+        {
+            sum += (tempCnpj[i] - '0') * multiplier2[i];
+        }
+            
+
+        remainder = sum % 11;
+        int digit2 = (remainder < 2) ? 0 : 11 - remainder;
+
+        return cnpj.EndsWith(digit1.ToString() + digit2.ToString());
+    }
+}
---
+++ src/DocumentValidators.CNPJ/Program.cs
@@ -0,0 +1,20 @@
+﻿using DocumentValidators.CNPJ.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** CNPJ validator **********");
+
+        Console.Write("Enter the CNPJ for validation: ");
+        string cnpj = Console.ReadLine();
+
+        bool isValid = CNPJModel.ValidateCnpj(cnpj);
+
+        Console.Write("---------- Result ----------\n");
+
+        Console.Write(isValid ? "Valid CNPJ!" : "Invalid CNPJ!");
+
+        Console.ReadKey();
+    }
+}
