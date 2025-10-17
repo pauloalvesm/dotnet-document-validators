68537f7f3f1e350b0e44a9ce1b63a48c3b451e21
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-16 22:00:23 -0300

    feat: apply CNPJ validation

--- src/DocumentValidators.CNPJ/Models/CNPJModel.cs
+++ src/DocumentValidators.CNPJ/Models/CNPJModel.cs
@@ -2,52 +2,34 @@
 
 public class CNPJModel
 {
-    public static bool ValidateCnpj(string cnpj)
+    private readonly string _cnpj;
+
+    public string Value => _cnpj;
+
+    public CNPJModel() { }
+
+    public CNPJModel(string cnpj)
     {
-        if (string.IsNullOrEmpty(cnpj)) 
+        if (string.IsNullOrWhiteSpace(cnpj))
         {
-            return false;
-        }
-            
-
-        cnpj = new string(cnpj.Where(char.IsDigit).ToArray());
-
-        if (cnpj.Length != 14) 
-        {
-            return false;
+            throw new ArgumentException("CNPJ cannot be null or empty.");
         }
 
-        if (cnpj.Distinct().Count() == 1) 
-        {
-            return false;
-        }
+        _cnpj = Clean(cnpj);
+    }
 
-        int[] multiplier1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
-        int[] multiplier2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+    private string Clean(string cnpj)
+    {
+        return new string(cnpj.Where(char.IsDigit).ToArray());
+    }
+    
+    public bool HasValidLength()
+    {
+        return _cnpj.Length == 14;
+    }
 
-        string tempCnpj = cnpj.Substring(0, 12);
-        int sum = 0;
-
-        for (int i = 0; i < 12; i++) 
-        {
-            sum += (tempCnpj[i] - '0') * multiplier1[i];
-        }
-            
-        int remainder = sum % 11;
-        int digit1 = (remainder < 2) ? 0 : 11 - remainder;
-
-        tempCnpj += digit1;
-        sum = 0;
-
-        for (int i = 0; i < 13; i++) 
-        {
-            sum += (tempCnpj[i] - '0') * multiplier2[i];
-        }
-            
-
-        remainder = sum % 11;
-        int digit2 = (remainder < 2) ? 0 : 11 - remainder;
-
-        return cnpj.EndsWith(digit1.ToString() + digit2.ToString());
+    public bool HasAllSameDigits()
+    {
+        return _cnpj.Distinct().Count() == 1;
     }
 }
--- src/DocumentValidators.CNPJ/Program.cs
+++ src/DocumentValidators.CNPJ/Program.cs
@@ -1,19 +1,32 @@
 ﻿using DocumentValidators.CNPJ.Models;
+using DocumentValidators.CNPJ.Validators;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("********** CNPJ validator **********");
+        Console.WriteLine("********** CNPJ Validator **********");
 
-        Console.Write("Enter the CNPJ for validation: ");
-        string cnpj = Console.ReadLine();
-
-        bool isValid = CNPJModel.ValidateCnpj(cnpj);
+        Console.Write("\nEnter the CNPJ for validation: ");
+        string input = Console.ReadLine();
 
         Console.Write("---------- Result ----------\n");
 
-        Console.Write(isValid ? "Valid CNPJ!" : "Invalid CNPJ!");
+        try
+        {
+            CNPJModel cnpj = new CNPJModel(input);
+
+            CNPJValidator validator = new CNPJValidator(cnpj);
+
+            bool isValid = validator.Validate();
+
+            Console.WriteLine(isValid ? "Valid CNPJ!" : "Invalid CNPJ!");
+        }
+        
+        catch (ArgumentException exception)
+        {
+            Console.Write($"Error: {exception.Message}");
+        }
 
         Console.ReadKey();
     }
