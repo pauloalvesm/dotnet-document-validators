1ff9e7edb09e0533b60cf82914db3d0f94131fa5
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-18 13:34:36 -0300

    feat: implement solution to validate RG

---
+++ src/DocumentValidators.RG/Validators/RGValidator.cs
@@ -0,0 +1,30 @@
+﻿using DocumentValidators.RG.Models;
+
+namespace DocumentValidators.RG.Validators;
+
+public class RGValidator
+{
+    public static bool Validate(RGModel model)
+    {
+        string rg = model.Rg;
+
+        if (string.IsNullOrEmpty(rg)) 
+        {
+            return false;
+        }
+            
+        rg = new string(rg.Where(char.IsDigit).ToArray());
+
+        if (rg.Length != 9) 
+        {
+            return false;
+        }
+
+        if (rg.Distinct().Count() == 1) 
+        {
+            return false;
+        }
+            
+        return true;
+    }
+}
